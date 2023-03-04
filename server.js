b1646a66dc9d7fdfe48a2f8cf0f4c0352be072fd
Harrison Tran-Chung <Tranchung@live.com>
2023-03-04 01:07:02 -0600

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const fs = require('fs');
 const { v4: uuid } = require('uuid');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Middleware for parsing JSON and urlencoded form data
